2b27f1faa564d26d3ed3e1ef55a719891232d9b3
GabrielLu73 <luquegabrielies@gmail.com>
2025-03-24 14:11:35 +0100

    services done

--- src/api/dailymenu/content-types/dailymenu/lifecycles.ts
+++ src/api/dailymenu/content-types/dailymenu/lifecycles.ts
@@ -1,16 +1,72 @@
+import { errors } from '@strapi/utils';
+const { ApplicationError } = errors;
+
 export default{
     beforeUpdate: async(event) => {
         const { data } = event.params;
 
         console.log("Daily menu :", {
             documentId: event.params.documentId,
-            datos: data.data,
+            datos: event.params.data,
         });
 
-        const priceMenu = await strapi.documents('api::dish.dish').findMany({
+        const priceMenu = await strapi.documents('api::dailymenu.dailymenu').findMany({
             filters: {
-                
+                day: event.params.data.day,
+            },
+            populate:{
+                first: {
+                    fields: 'price',
+                },
+                second: {
+                    fields: 'price',
+                },
+                dessert: {
+                    fields: 'price',
+                }
             }
         });
+
+        const { first, second, dessert } = priceMenu[0];
+
+        const result = (first?.price ?? 0) + (second?.price ?? 0) + (dessert?.price ?? 0);
+
+        data.sum_price = result;
+
+        //Ensure that a dish is not repeated
     },
+
+    afterUpdate: async( event ) => {
+
+        const repit = await strapi.documents('api::dailymenu.dailymenu').findMany({
+            filters: {
+                day: event.params.data.day,
+            },
+            populate:{
+                first: {
+                    fields: 'price',
+                },
+                second: {
+                    fields: 'price',
+                },
+                dessert: {
+                    fields: 'price',
+                }
+            }
+        });
+
+        const { first, second, dessert } = repit[0];
+
+        if (first && second && first.id === second.id) {
+            throw new ApplicationError('El mismo plato no puede ser usado como primero y segundo');
+        }
+          
+        if (first && dessert && first.id === dessert.id) {
+            throw new ApplicationError('El mismo plato no puede ser usado como primero y postre');
+        }
+          
+        if (second && dessert && second.id === dessert.id) {
+            throw new ApplicationError('El mismo plato no puede ser usado como segundo y postre');
+        }
+    }
 }
